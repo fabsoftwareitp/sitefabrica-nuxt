770d07adcc81e2476f4be407f9c290b4df8f61c0
danilocbueno <danilocbueno@gmail.com>
2022-07-12 17:38:37 -0300

    alterações no layout da pagina inicial

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="flex justify-between max-w-6xl mx-auto py-8">
+  <div class="flex justify-around max-w-6xl py-8">
     <!-- Navigation -->
-    <pre>{{ navigation }}</pre>
     <div class="text-white dark:text-gray-200">
-      <h3 class="text-3xl font-semibold">
         <NuxtLink to="/" class="mr-6" active-class="font-bold text-orange-600 underline">Home</NuxtLink>
         <ContentNavigation v-slot="{ navigation }">
           <NuxtLink
@@ -11,15 +9,14 @@
             :key="link._path"
             :to="link._path"
             active-class="font-bold text-orange-600 underline"
-            class="mr-6"
+            class="mr-6 font-semibold"
           >
             {{ link.navTitle || link.title }}
           </NuxtLink>
         </ContentNavigation>
-      </h3>
     </div>
     <!-- Social icons & Color Mode -->
-    <div class="space-x-3 text-gray-500 transition">
+    <div class="space-x-3 text-gray-200 transition">
       <a href="https://github.com/fabsoftwareitp" title="GitHub" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:github" /></a>
       <ColorModeSwitch class="hover:text-gray-700 dark:hover:text-gray-300" />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="imagem-fundo">
     <Navbar />
-    <div class="flex justify-center items-center h-5/6">
-      <img src="../public/logo-oficial.png" alt="Logo Fábrica de Software" class="w-1/6">
-      <div class="pl-24">
-        <h1 class="inline-block text-6xl sm:text-7xl font-semibold text-slate-900 tracking-tight dark:text-slate-200">
-          Fábrica de Software
-        </h1>
-        <h2 class="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-orange-400 to-[#FD624C] text-right">
+    <div class="mt-12 mx-auto h-5/6">
+      <div class="relative">
+      <img src="/logo-oficial.png" alt="Logo Fábrica de Software" class=" d-block mx-auto w-56 drop-shadow-2xl">
+      <img src="/logo-oficial.png" alt="Logo Fábrica de Software" class="absolute top-0 left-0 right-0 mx-auto w-56 hover:animate-ping">
+      </div>
+      <div>
+        <h1 class="mt-12 text-center text-6xl sm:text-7xl font-semibold text-slate-900 tracking-tight dark:text-slate-200">Fábrica de Software</h1>
+
+        <h2 class="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-orange-400 to-[#FD624C] text-center">
           IFSP Itapetininga
         </h2>
       </div>
